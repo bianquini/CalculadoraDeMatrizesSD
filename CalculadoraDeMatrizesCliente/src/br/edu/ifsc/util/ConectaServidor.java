474bb13baee89dc4741edba89d5b38a7fe6099d5
aluno <aluno@debian>
2019-09-23 08:41:42 -0300

    Cliente: Ajustes na conexão com o servidor

--- CalculadoraDeMatrizesCliente/src/br/edu/ifsc/util/ConectaServidor.java
+++ CalculadoraDeMatrizesCliente/src/br/edu/ifsc/util/ConectaServidor.java
@@ -5,7 +5,7 @@ import java.rmi.Naming;
 import br.edu.ifsc.interfaces.ICalculadoraMatrizes;
 
 /**
- * Realiza a conexão com o servidor para realizar as multiplicações
+ * Realiza a conexï¿½o com o servidor para realizar as multiplicaï¿½ï¿½es
  * 
  * @author Tiago
  *
@@ -17,14 +17,14 @@ public class ConectaServidor {
 
 	/**
 	 * 
-	 * Realiza a conexão com o servidor para realizar a multiplicação das matrizes
+	 * Realiza a conexï¿½o com o servidor para realizar a multiplicaï¿½ï¿½o das matrizes
 	 * 
-	 * @param caminho        - Caminho de onde está localizado o servidor
+	 * @param caminho        - Caminho de onde estï¿½ localizado o servidor
 	 * @param matrizQuebrada - Matriz contendo uma parte da matriz A
-	 * @param matB           - Matriz B que será multiplicada
+	 * @param matB           - Matriz B que serï¿½ multiplicada
 	 * 
 	 * @return matriz multiplicada com tamanho 1056x4096
-	 * @throws Exceção no caso de erro na multiplicação da matriz
+	 * @throws Exceï¿½ï¿½o no caso de erro na multiplicaï¿½ï¿½o da matriz
 	 * 
 	 */
 	public long[][] conectar(String caminho, long[][] matrizQuebrada, long[][] matB) {
@@ -32,11 +32,13 @@ public class ConectaServidor {
 			@Override
 			public void run() {
 				try {
-					ICalculadoraMatrizes calc = (ICalculadoraMatrizes) Naming.lookup(caminho);
+					ICalculadoraMatrizes calc;
+					System.setSecurityManager(new SecurityManager());
+					calc = (ICalculadoraMatrizes) Naming.lookup(caminho);
 					matriz = calc.mult(matrizQuebrada, matB);
 					// return matriz;
 				} catch (Exception e) {
-					System.err.println("\tErro: " + e.getMessage());
+					System.err.println("\tErro no Servidor: " + e.getMessage());
 					System.exit(1);
 				}
 				// return matriz;
